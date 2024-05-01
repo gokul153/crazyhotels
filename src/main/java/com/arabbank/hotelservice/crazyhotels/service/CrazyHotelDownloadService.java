a4b9f7581d9680e5a240cdeefb41eb0a8ec86815
Dassapan <97041573+gokul153@users.noreply.github.com>
2024-05-01 16:05:23 +0530

    final chanes for mocking

--- src/main/java/com/arabbank/hotelservice/crazyhotels/service/CrazyHotelDownloadService.java
+++ src/main/java/com/arabbank/hotelservice/crazyhotels/service/CrazyHotelDownloadService.java
@@ -1,4 +1,15 @@
 package com.arabbank.hotelservice.crazyhotels.service;
 
+import com.arabbank.hotelservice.crazyhotels.model.entity.CrazyHotelEntity;
+import com.arabbank.hotelservice.crazyhotels.repository.CrazyHotelEntityRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CrazyHotelDownloadService {
+    @Autowired
+    public CrazyHotelEntityRepo crazyHotelEntityRepo;
+
+
+
 }
